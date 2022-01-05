09e3b34cadfd8d8f1afa81a94918ef337a4f4117
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2022-01-05 17:09:56 -0300

    agora ta file

--- lib/item_model.dart
+++ lib/item_model.dart
@@ -10,4 +10,16 @@ class Item {
     required this.price,
     required this.date,
   });
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    final properties = map['properties'] as Map<String, dynamic>;
+    final nameList = (properties['Name']?['title'] ?? []) as List;
+    final dateStr = properties['Date']?['date']?['start'];
+    return Item(
+      name: nameList.isNotEmpty ? nameList[0]['plain_text'] : '?',
+      category: properties['Category']?['select']?['name'] ?? 'Any',
+      price: (properties['Price']?['number'] ?? 0).toDouble(),
+      date: dateStr != null ? DateTime.parse(dateStr) : DateTime.now(),
+    );
+  }
 }
